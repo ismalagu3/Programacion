597ecee6948d728e06e8bb436f159e4b3666940d
ismalagu3 <ismalagu3@gmail.com>
2022-12-13 19:26:24 +0100

    todo ok

--- WEB/web_tareas/conexion.php
+++
@@ -1,7 +0,0 @@
-<?php
-$mysqli=new mysqli("sql7.freemysqlhosting.net","sql7564092","f*%hzjac&ivYD#U#","sql7564092");
-$res=$mysqli->query("select * from web where usuario='$usuario'");
-$fila=$res->fetch_assoc();
-$res->close();
-$mysqli->close();
-?>
--- WEB/web_tareas/insertar.php
+++
@@ -1,7 +0,0 @@
-<?php   
-$tarea=$_POST['tarea'];
-
-$mysqli=new mysqli("sql7.freemysqlhosting.net","sql7564092","SlyBea9HlB","sql7564092");
-$query="INSERT INTO tarea (tarea) VALUES ('$tarea')";
-$res=$mysqli->query($query);
-?>
